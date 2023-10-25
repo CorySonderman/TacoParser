7474f38e6c34af20b9ab01c12614f70eb683f3ff
CorySonderman <sonderman2000@yahoo.com>
2023-10-25 10:17:18 -0500

    Added the 2 closest Taco Bells

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -34,10 +34,16 @@ namespace LoggingKata
 
             // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other. -DONE
            
-            ITrackable tacoBell1 = new TacoBell();
-            ITrackable tacoBell2 = new TacoBell();
+            ITrackable closestTacoBell1 = new TacoBell();
+            ITrackable closestTacoBell2 = new TacoBell();
+            ITrackable furthestTacoBell1 = new TacoBell();
+            ITrackable furthestTacoBell2 = new TacoBell();
+
+
             // Create a `double` variable to store the distance
-            double distance = 0;
+            //double distance = 0;
+            double closestDistance = double.MaxValue;
+            double furthestDistance = double.MinValue;
             // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;` -DONE
 
             //HINT NESTED LOOPS SECTION---------------------
@@ -53,38 +59,65 @@ namespace LoggingKata
             for (int i = 0; i < locations.Length; i++)
             {
                 var locA = locations[i];
-                var corA = new GeoCoordinate();
-                corA.Latitude = locA.Location.Latitude; 
-                corA.Longitude = locA.Location.Longitude;
+                var corA = new GeoCoordinate(locA.Location.Latitude, locA.Location.Longitude);
 
                 for (int h = 0; h < locations.Length; h++)
                 {
+                    if (i == h)
+                    {
+                        continue; // Skip comparing a location to itself
+                    }
+
                     var locB = locations[h];
                     var corB = new GeoCoordinate(locB.Location.Latitude, locB.Location.Longitude);
 
-                    if(corA.GetDistanceTo(corB) > distance)
+                    double currentDistance = corA.GetDistanceTo(corB);
+
+                    if (currentDistance < closestDistance)
                     {
-                        distance = corA.GetDistanceTo(corB);
-                        tacoBell1 = locA; tacoBell2 = locB;
+                        closestDistance = currentDistance;
+                        closestTacoBell1 = locA;
+                        closestTacoBell2 = locB;
+                    }
+
+                    if (currentDistance > furthestDistance)
+                    {
+                        furthestDistance = currentDistance;
+                        furthestTacoBell1 = locA;
+                        furthestTacoBell2 = locB;
                     }
                 }
             }
+
             // Once you've looped through everything, you've found the two Taco Bells farthest away from each other. - DONE
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("IT'S RAINING TACOS!");
             Console.ResetColor();
 
-            Console.ForegroundColor= ConsoleColor.DarkGreen;
-            Console.Write($"{tacoBell1.Name}");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write($"{furthestTacoBell1.Name}");
             Console.ResetColor();
             Console.Write(" and ");
-            Console.ForegroundColor= ConsoleColor.DarkGreen;
-            Console.Write($"{tacoBell2.Name}");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write($"{furthestTacoBell2.Name}");
             Console.ResetColor();
             Console.WriteLine($"have the farthest distance between them.");
-            Console.WriteLine($"They are {Math.Round(distance / 1609.34)} miles apart.");
-            Console.ForegroundColor= ConsoleColor.DarkRed;
+            Console.WriteLine($"They are {Math.Round(furthestDistance / 1609.34)} miles apart.");
+            Console.WriteLine();
+            Console.WriteLine("But why drive all that way and waste your gas? ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write($"{closestTacoBell1.Name}");
+            Console.ResetColor();
+            Console.Write(" and ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write($"{closestTacoBell2.Name}");
+            Console.ResetColor();
+            
+            Console.WriteLine($"have the closest distance between them.");
+            Console.WriteLine($"They are {Math.Round(closestDistance / 1609.34)} miles apart.");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("Can I get some hot sauce for my hot sauce?");
             Console.ResetColor();
             Console.WriteLine();
